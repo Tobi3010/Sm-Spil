ec0db98f0d5adcabf1139a6ec2345bd982557d10
Tobi3010 <tobiasandersen62@gmail.com>
2020-09-04 09:40:05 +0200

    lel

--- StenSaksPapir.py
+++ StenSaksPapir.py
@@ -2,15 +2,28 @@ import random
 
 print("Sten Saks Papir!")
 
-
-
-
 i = ["Saks", "Sten"]
 n = ["Sten", "Papir"]
 k = ["Papir", "Saks"]
 List = [i,n,k]
 
-Enemy = List[random.randint(0, 2)][0]
-print(Enemy)
+EnemyPoints = 0
+PlayerPoints = 0
+game = True
 
-print(List[0][1])
+while game == True:
+    Enemy = List[random.randint(0, 2)][0]
+    print(Enemy)
+    Player = input("Hvad spiller du? ")
+
+    for x in range(3):
+        if Player in List[x] and Enemy in List[x]:
+            if List[x][0] == Enemy and Player != Enemy:
+                PlayerPoints = PlayerPoints + 1
+                print("Player won!")
+            elif List[x][0] == Player and Player != Enemy:
+                EnemyPoints = EnemyPoints + 1
+                print("Enemy won!")
+    if EnemyPoints == 3 or PlayerPoints == 3:
+        game = False
+print("GAME OVER")
